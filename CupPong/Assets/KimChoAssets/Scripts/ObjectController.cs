b1b946f07c2b5496784141411034b3751f247102
kimdav011 <kimdav011@gmail.com>
2020-04-10 14:25:42 -0700

    update

--- CupPong/Assets/KimChoAssets/Scripts/ObjectController.cs
+++ CupPong/Assets/KimChoAssets/Scripts/ObjectController.cs
@@ -7,10 +7,14 @@ public class ObjectController : MonoBehaviour
     public ASL.ASLObject thisASLObject;
     public ASL.ASLObject aslObjectToSyncWith;
     public GameObject objectToSyncWith;
+    public SimpleDemos.SendFloatArray_Example floatObject;
 
     // Update is called once per frame
     void Update()
     {
+
+        handleObjectKinematic();
+
         // If the object is ours, then do nothing with it.
         if (thisASLObject.m_Mine)
         {
@@ -21,5 +25,37 @@ public class ObjectController : MonoBehaviour
         this.transform.position = objectToSyncWith.transform.position;
         this.transform.rotation = objectToSyncWith.transform.rotation;
         this.transform.localScale = objectToSyncWith.transform.localScale + (objectToSyncWith.transform.localScale * 0.1f);
+    }
+
+    public void makeKinematic()
+    {
+        this.floatObject.m_MyFloats[0] = 1.0f;
+        this.floatObject.m_SendFloat = true;
+    }
+
+    public void releaseKinematic()
+    {
+        this.floatObject.m_MyFloats[0] = 0.0f;
+        this.floatObject.m_SendFloat = true;
+    }
+
+    public void handleObjectKinematic()
+    {
+        if (floatObject != null)
+        {
+            if (this.gameObject.GetComponent<Collider>() != null)
+            {
+                if (this.gameObject.GetComponent<Rigidbody>() != null)
+                {
+                    if (floatObject.m_MyFloats[0] > 0.9 && floatObject.m_MyFloats[0] < 1.1)
+                    {
+                        this.gameObject.GetComponent<Rigidbody>().isKinematic = true;
+                    } else
+                    {
+                        this.gameObject.GetComponent<Rigidbody>().isKinematic = false;
+                    }
+                }
+            }
+        }
     }
 }
