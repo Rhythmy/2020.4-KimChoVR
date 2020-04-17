d0a643d1bf9f1b9b356e453b6f98c56f4322d209
kimdav011 <kimdav011@gmail.com>
2020-04-17 00:47:24 -0700

    update

--- CupPong/Assets/KimChoAssets/Scripts/DestroyOnTouch.cs
+++ CupPong/Assets/KimChoAssets/Scripts/DestroyOnTouch.cs
@@ -5,10 +5,22 @@ using UnityEngine;
 public class DestroyOnTouch : MonoBehaviour
 {
     public GameObject parent;
+    public SimpleDemos.DeleteObject_Example deleteScript;
 
     private void OnTriggerEnter(Collider other)
     {
-        Destroy(other.gameObject);
-        Destroy(parent);
+        if (other.gameObject == parent)
+        {
+            return;
+        }
+
+        if (other.tag == "Grabbable")
+        {
+            if (other.GetComponent<SimpleDemos.DeleteObject_Example>() != null)
+            {
+                other.GetComponent<SimpleDemos.DeleteObject_Example>().m_Delete = true;
+            }
+            deleteScript.m_Delete = true;
+        }
     }
 }
